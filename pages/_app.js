852ebf6b2a2ebb2efbf7761a43efd6660e08e7d4
WildanFajarMaulana <wildanfajar71@gmail.com>
2022-04-26 08:37:11 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Script from "next/script";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossOrigin="anonymous" />
       </Head>
 
       <Component {...pageProps} />
